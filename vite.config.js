bfc2a1b5a9d259bb495870fc6eb0e51f6375fcf9
gabrielbermudeztoro-lab <gabriel.bermudez.toro@gmail.com>
2026-02-18 12:28:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',        // raíz del proyecto
   build: {
-    outDir: 'dist',  // carpeta de salida
     rollupOptions: {
-      input: 'index.html'  // entrada apuntando a raíz
+      input: 'index.html'
     }
   }
 })
